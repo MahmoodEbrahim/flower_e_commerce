7e7cd1154f380db976d410ee9cd88108deea02ce
MahmoodEbrahim <m7kmod7k2003@gmail.com>
2025-08-16 18:31:27 +0300

    edit di modules

--- lib/core/di/di.config.dart
+++ lib/core/di/di.config.dart
@@ -8,8 +8,11 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:dio/dio.dart' as _i361;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
+
+import 'modules/dio_modules.dart' as _i288;
 
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -17,11 +20,15 @@ extension GetItInjectableX on _i174.GetIt {
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) {
-    _i526.GetItHelper(
+    final gh = _i526.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
+    final dioModule = _$DioModule();
+    gh.lazySingleton<_i361.Dio>(() => dioModule.dio);
     return this;
   }
 }
+
+class _$DioModule extends _i288.DioModule {}
