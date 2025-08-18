a8a7cce59a851a9ef356b225ccc32ea952b66453
Rana_Gebril <ranagebril5@gmail.com>
2025-08-18 20:01:49 +0300

    feat: add LoginUsecase

---
+++ lib/features/auth/domain/usecase/login_usecase.dart
@@ -0,0 +1,14 @@
+import 'package:flower_e_commerce/core/utils/api_result/api_result.dart';
+import 'package:flower_e_commerce/features/auth/domain/entity/login_entity.dart';
+import 'package:flower_e_commerce/features/auth/domain/repository/auth_repository.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable()
+class LoginUsecase {
+  AuthRepository authRepository;
+  LoginUsecase(this.authRepository);
+
+  Future<ApiResult<LoginEntity>> call(String email, String password) async {
+    return await authRepository.login(email, password);
+  }
+}
