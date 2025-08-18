c28646808326a757d421a1989f66483a17c37ab4
Rana_Gebril <ranagebril5@gmail.com>
2025-08-18 19:14:31 +0300

    feat(api): implement login endpoint in AuthApiService

--- lib/features/auth/api/client/auth_api_service.dart
+++ lib/features/auth/api/client/auth_api_service.dart
@@ -1,18 +1,17 @@
-
 import 'package:dio/dio.dart';
 import 'package:flower_e_commerce/core/utils/constants/api_end_points.dart';
+import 'package:flower_e_commerce/features/auth/api/models/responses/login_response.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'auth_api_service.g.dart';
 
-@RestApi(baseUrl: ApiEndPoints.apiUrl+ApiEndPoints.authEndPoint)
+@RestApi(baseUrl: ApiEndPoints.apiUrl + ApiEndPoints.authEndPoint)
 @injectable
 abstract class AuthApiService {
-
   @factoryMethod
   factory AuthApiService(Dio dio) = _AuthApiService;
 
-
-
+  @POST(ApiEndPoints.loginEndPoint)
+  Future<LoginResponse> logIn(@Body() Map<String, dynamic> body);
 }
