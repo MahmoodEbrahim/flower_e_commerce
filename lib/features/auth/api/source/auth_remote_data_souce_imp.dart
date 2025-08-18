525f78efeff62b938cf1578dcbf7298483f774d2
Rana_Gebril <ranagebril5@gmail.com>
2025-08-18 19:19:27 +0300

    feat: implement login in AuthRemoteDataSource

--- lib/features/auth/api/source/auth_remote_data_souce_imp.dart
+++ lib/features/auth/api/source/auth_remote_data_souce_imp.dart
@@ -1,6 +1,30 @@
+import 'package:dio/dio.dart';
+import 'package:flower_e_commerce/core/utils/api_result/api_result.dart';
+import 'package:flower_e_commerce/features/auth/api/client/auth_api_service.dart';
+import 'package:flower_e_commerce/features/auth/api/models/responses/login_response.dart';
 import 'package:flower_e_commerce/features/auth/data/source/auth_remote_data_source.dart';
+import 'package:flower_e_commerce/features/auth/domain/entity/login_entity.dart';
 import 'package:injectable/injectable.dart';
-@Injectable(as: AuthRemoteDataSource)
-class AuthRemoteDataSouceImp  implements AuthRemoteDataSource{
 
+@Injectable(as: AuthRemoteDataSource)
+class AuthRemoteDataSourceImp implements AuthRemoteDataSource {
+  AuthApiService authApiService;
+
+  AuthRemoteDataSourceImp(this.authApiService);
+
+  @override
+  Future<ApiResult<LoginEntity>> login(String email, String password) async {
+    try {
+      LoginResponse response =
+          await authApiService.logIn({"email": email, "password": password});
+
+      return ApiSuccessResult<LoginEntity>(response.toLoginEntity());
+    } catch (e) {
+      if (e is DioException) {
+        return ApiErrorResult<LoginEntity>(e.message ?? e.toString());
+      }
+      return ApiErrorResult<LoginEntity>(
+          e.toString()); // T is signature means --> error for logon entity
+    }
+  }
 }
--- lib/features/auth/data/source/auth_remote_data_source.dart
+++ lib/features/auth/data/source/auth_remote_data_source.dart
@@ -1,3 +1,6 @@
+import 'package:flower_e_commerce/core/utils/api_result/api_result.dart';
+import 'package:flower_e_commerce/features/auth/domain/entity/login_entity.dart';
+
 abstract interface class AuthRemoteDataSource {
-  
+  Future<ApiResult<LoginEntity>> login(String email, String password);
 }
