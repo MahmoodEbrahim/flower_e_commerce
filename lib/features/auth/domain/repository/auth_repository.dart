49fc45aba209e845f04d6b50706e329f84e1159e
Rana_Gebril <ranagebril5@gmail.com>
2025-08-18 19:30:55 +0300

    feat: implement login in AuthRepository

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -1,3 +1,8 @@
+import 'package:flower_e_commerce/core/utils/api_result/api_result.dart';
+import 'package:flower_e_commerce/features/auth/domain/entity/login_entity.dart';
+import 'package:flower_e_commerce/features/auth/domain/entity/user_entity.dart';
+
 abstract interface class AuthRepository {
+  Future<ApiResult<LoginEntity>> login(String email,String password);
   
 }
