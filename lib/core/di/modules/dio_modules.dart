50af68f83b3e5fa9a87519f87c26b3afe2fadb5c
Rana_Gebril <ranagebril5@gmail.com>
2025-08-21 02:59:24 +0300

    add token interceptor

--- lib/core/di/modules/dio_modules.dart
+++ lib/core/di/modules/dio_modules.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flower_e_commerce/core/network/token_interceptor.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
@@ -8,6 +9,7 @@ abstract class DioModule {
   Dio dio(PrettyDioLogger logger) {
     final dio = Dio();
     dio.interceptors.add(logger);
+    dio.interceptors.add(TokenInterceptor());
     return dio;
   }
 
