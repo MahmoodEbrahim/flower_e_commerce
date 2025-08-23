baa0525d46c41ddc6ac0089a71e6d098a0276cb5
Aya Mohamed <aya44saber@gmail.com>
2025-08-23 23:29:28 +0300

    refactor dependency injection setup for clarity and organization

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -1,14 +1,10 @@
-// import 'package:get_it/get_it.dart';
-// import 'package:injectable/injectable.dart';
-// import 'di.config.dart';
-//
-// final getIt = GetIt.instance;
-//
-// @InjectableInit(
-//   initializerName: 'init',
-//   preferRelativeImports: true,
-//   asExtension: true,
-// )
-// void configureDependencies() {
-//   getIt.init();
-// }
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+ import 'di.config.dart';
+final getIt = GetIt.instance;
+@InjectableInit(
+  initializerName: 'init',
+  preferRelativeImports: true,
+  asExtension: true,
+)
+Future<void> configureDependencies() async => getIt.init();
