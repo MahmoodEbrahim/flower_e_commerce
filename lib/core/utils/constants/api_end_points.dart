80377fe5c0dff16509eb6a51cb746de47283dfa4
MahmoodEbrahim <m7kmod7k2003@gmail.com>
2025-08-24 14:05:06 +0300

    update

--- lib/core/utils/constants/api_end_points.dart
+++ lib/core/utils/constants/api_end_points.dart
@@ -1,4 +1,7 @@
 abstract class ApiEndPoints {
   static const String apiUrl = "https://flower.elevateegy.com/api/v1/";
- 
+  static const String authEndPoint = "auth/";
+  static const String forgetPasswordEndPoint = "forgotPassword";
+  static const String verfiyPasswordEndPoint = "verifyResetCode";
+  static const String resetPasswordEndPoint = "resetPassword";
 }
