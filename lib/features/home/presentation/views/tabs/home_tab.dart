868c7a70f7fb438480ba38c02fea6ca29da68b78
MahmoodEbrahim <m7kmod7k2003@gmail.com>
2025-08-30 17:42:00 +0300

    loading animation

--- lib/features/home/presentation/views/tabs/home_tab.dart
+++ lib/features/home/presentation/views/tabs/home_tab.dart
@@ -27,14 +27,16 @@ class HomeTab extends StatelessWidget {
           builder: (context, state) {
             if (state is HomeInitialState) {
               BlocProvider.of<HomeBloc>(context).add(GetHomeDataEvent());
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:
-                [
-                  SizedBox(height: 200.0,),
-                  LoadingAnimationWidget.inkDrop(color: AppColors.Pink,size: 50)
-                ],);
+              return Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:
+                  [
+                    SizedBox(height: 200.0,),
+                    LoadingAnimationWidget.inkDrop(color: AppColors.Pink,size: 50)
+                  ],),
+              );
             } else if (state is HomeLoadingState) {
               return Center(
                 child: Column(
