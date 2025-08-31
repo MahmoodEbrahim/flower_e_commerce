fdaa73b9bffc468376251ec65efe96378b4c874a
Rana_Gebril <ranagebril5@gmail.com>
2025-09-01 00:52:01 +0300

    extend body behind app bar

--- lib/features/home/presentation/views/pages/products_details_page.dart
+++ lib/features/home/presentation/views/pages/products_details_page.dart
@@ -15,53 +15,60 @@ class ProductDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final locale = AppLocalizations.of(context)!;
     return Scaffold(
-      backgroundColor: AppColors.White,
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(
-            backgroundColor: AppColors.lightPink,
-            floating: true,
-            leading: GestureDetector(
-              child: Icon(Icons.arrow_back_ios),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ), // custom icon
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              color: AppColors.lightPink,
-              height: MediaQuery.of(context).size.height * 0.5,
-              child: Column(
-                children: [
-                  Expanded(
-                    child: PageView(
+        SliverAppBar(
+        backgroundColor: AppColors.lightPink,
+        expandedHeight: MediaQuery.of(context).size.height * 0.5,
+        pinned: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios,size: 35,),
+          onPressed: () => Navigator.pop(context),
+        ),
+        flexibleSpace: FlexibleSpaceBar(
+          background: Container(
+            color: AppColors.lightPink,
+            margin: EdgeInsets.only(top: 50),
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: Column(
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    width: 300,
+                    height: 550,
+                    child:  PageView(
                       controller: controller,
                       children: product!.images!
                           .map(
                             (imageUrl) =>
-                            Image.network(imageUrl, fit: BoxFit.contain),
+                                AspectRatio(
+                                  aspectRatio: 4/3,
+                                  child: Image.network(imageUrl, fit: BoxFit.contain),
+                                )
                       )
                           .toList(),
                     ),
+                  )
+                ),
+                SizedBox(height: 8),
+                SmoothPageIndicator(
+                  controller: controller,
+                  count: product!.images!.length,
+                  effect: ScrollingDotsEffect(
+                    dotHeight: 10,
+                    dotWidth: 10,
+                    dotColor: AppColors.white[70] ?? AppColors.white,
+                    activeDotColor: AppColors.Pink,
+                    activeDotScale: 1.3,
                   ),
-                  SizedBox(height: 8),
-                  SmoothPageIndicator(
-                    controller: controller,
-                    count: product!.images!.length,
-                    effect: ScrollingDotsEffect(
-                      dotHeight: 10,
-                      dotWidth: 10,
-                      dotColor: AppColors.white[70] ?? AppColors.white,
-                      activeDotColor: AppColors.Pink,
-                      activeDotScale: 1.3,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                ],
-              ),
+                ),
+                SizedBox(height: 8),
+              ],
             ),
           ),
+        ),
+      ),
+
           SliverToBoxAdapter(
             child: Padding(
               padding: EdgeInsets.all(15),
