45bc7cc66141ae81a2d74a7650de23e19995e368
mariam2MOHMED <mariammohmed.25720@gmail.com>
2025-09-05 15:16:29 +0300

    solve an issue

--- lib/features/profile/presentation/views/pages/terms_and_condition_page.dart
+++ lib/features/profile/presentation/views/pages/terms_and_condition_page.dart
@@ -8,14 +8,75 @@ import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import '../../../../../core/utils/json_helpers/json_loader.dart';
 
+// class TermsAndConditionPage extends StatelessWidget {
+//   final String locale;
+//
+//   const TermsAndConditionPage({super.key, this.locale = JsonKeys.en});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final t = AppLocalizations.of(context)!;
+//     final currentLocale = Localizations.localeOf(context).languageCode;
+//
+//     return FutureBuilder<Map<String, dynamic>>(
+//       future: loadJson(JsonAssets.terms),
+//       builder: (context, snapshot) {
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return Scaffold(
+//             backgroundColor: AppColors.white,
+//             body: Center(
+//               child: LoadingAnimationWidget.inkDrop(
+//                 color: AppColors.pink,
+//                 size: 50,
+//               ),
+//             ),
+//           );
+//         }
+//
+//         if (!snapshot.hasData || snapshot.data == null) {
+//           return Center(child: Text(t.noDataFound));
+//         }
+//
+//         if (snapshot.hasError) {
+//           return Center(child: Text(t.errorLoadingData));
+//         }
+//
+//         final data = snapshot.data!;
+//         final sectionsJsonRaw = data[JsonKeys.terms];
+//         final sectionsJson = sectionsJsonRaw is List ? sectionsJsonRaw : null;
+//
+//         if (sectionsJson == null) {
+//           return Center(child: Text(t.noSectionsFound));
+//         }
+//
+//         final sections =
+//             sectionsJson.map((e) => GenericSectionModel.fromJson(e)).toList();
+//
+//         return Scaffold(
+//           extendBodyBehindAppBar: true,
+//           body: Padding(
+//             padding: const EdgeInsetsDirectional.only(top: 20),
+//             child: SingleChildScrollView(
+//               child: Column(
+//                 children: sections
+//                     .map((section) => buildGenericSection(section, locale))
+//                     .toList(),
+//               ),
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
 class TermsAndConditionPage extends StatelessWidget {
-  final String locale;
-
-  const TermsAndConditionPage({super.key, this.locale = JsonKeys.en});
+  const TermsAndConditionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     final t = AppLocalizations.of(context)!;
+    final currentLocale = Localizations.localeOf(context).languageCode; // <-- هنا
+
     return FutureBuilder<Map<String, dynamic>>(
       future: loadJson(JsonAssets.terms),
       builder: (context, snapshot) {
@@ -48,16 +109,30 @@ class TermsAndConditionPage extends StatelessWidget {
         }
 
         final sections =
-            sectionsJson.map((e) => GenericSectionModel.fromJson(e)).toList();
-
+        sectionsJson.map((e) => GenericSectionModel.fromJson(e)).toList();
+        final firstSection = sections.first; // أول سيكشن
+        final otherSections = sections.skip(1).toList();
         return Scaffold(
           extendBodyBehindAppBar: true,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+elevation: 0,
+
+              shadowColor: Colors.transparent,
+              leading: IconButton(
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.transparent,
+                  size: 24,
+                ),
+                onPressed: () => Navigator.pop(context),
+              ),),
           body: Padding(
             padding: const EdgeInsetsDirectional.only(top: 20),
             child: SingleChildScrollView(
               child: Column(
                 children: sections
-                    .map((section) => buildGenericSection(section, locale))
+                    .map((section) => buildGenericSection(section, currentLocale)) // <-- هنا
                     .toList(),
               ),
             ),
